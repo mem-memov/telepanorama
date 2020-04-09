5937362489aa6a676ca5dd7d59bbe2f9530921a4
Aleksandr Novikov <mem-memov@yandex.ru>
2020-04-09 10:04:27 +0300

    php imap

--- decorator/src/MailController.php
+++ decorator/src/MailController.php
@@ -2,6 +2,8 @@
 
 namespace Telepanorama;
 
+use PhpImap\Mailbox;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -9,6 +11,14 @@ class MailController
 {
     public function get(Request $request, Response $response): Response
     {
+        $mailbox = new Mailbox(
+            '{imap.gmail.com:993/imap/ssl}INBOX', // IMAP server and mailbox folder
+            'telepanorama@gmail.com', // Username for the before configured mailbox
+            'No276113@', // Password for the before configured username
+            '/tmp', // Directory, where attachments will be saved (optional)
+            'UTF-8' // Server encoding (optional)
+        );
+
         return $response;
     }
 }

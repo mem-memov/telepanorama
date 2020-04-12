9cff138408af6f46dd1dcf7f80b65ceff7262b2d
Aleksandr Novikov <mem-memov@yandex.ru>
2020-04-12 12:18:46 +0300

    smtp send

--- decorator/src/Partner/PostOffice/Smtp/Connection.php
+++ decorator/src/Partner/PostOffice/Smtp/Connection.php
@@ -1,10 +1,74 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Telepanorama\Partner\PostOffice\Smtp;
 
+use PHPMailer\PHPMailer\PHPMailer;
 
 class Connection
 {
+    public function sendMessage(): void
+    {
+        //Create a new PHPMailer instance
+        $mail = new PHPMailer;
 
+        //Tell PHPMailer to use SMTP
+        $mail->isSMTP();
+
+        //Enable SMTP debugging
+        // SMTP::DEBUG_OFF = off (for production use)
+        // SMTP::DEBUG_CLIENT = client messages
+        // SMTP::DEBUG_SERVER = client and server messages
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+        //Set the hostname of the mail server
+        $mail->Host = 'smtp.gmail.com';
+        // use
+        // $mail->Host = gethostbyname('smtp.gmail.com');
+        // if your network does not support SMTP over IPv6
+
+        //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+        $mail->Port = 587;
+
+        //Set the encryption mechanism to use - STARTTLS or SMTPS
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+        //Whether to use SMTP authentication
+        $mail->SMTPAuth = true;
+
+        //Username to use for SMTP authentication - use full email address for gmail
+        $mail->Username = 'username@gmail.com';
+
+        //Password to use for SMTP authentication
+        $mail->Password = 'yourpassword';
+
+        //Set who the message is to be sent from
+        $mail->setFrom('from@example.com', 'First Last');
+
+        //Set an alternative reply-to address
+        $mail->addReplyTo('replyto@example.com', 'First Last');
+
+        //Set who the message is to be sent to
+        $mail->addAddress('whoto@example.com', 'John Doe');
+
+        //Set the subject line
+        $mail->Subject = 'PHPMailer GMail SMTP test';
+
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+
+        //Replace the plain text body with one created manually
+        $mail->AltBody = 'This is a plain-text message body';
+
+        //Attach an image file
+        $mail->addAttachment('images/phpmailer_mini.png');
+
+        $isSent = $mail->send();
+
+        if (false === $isSent) {
+            throw new SendFailed('Mailer Error: '. $mail->ErrorInfo);
+        }
+    }
 }
---
+++ decorator/src/Partner/PostOffice/Smtp/SendFailed.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Telepanorama\Partner\PostOffice\Smtp;
+
+class SendFailed extends Problem
+{
+
+}

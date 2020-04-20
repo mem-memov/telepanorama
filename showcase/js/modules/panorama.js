0289a72bfbec84c466bd10f94eabf7ecb8d7ebd9
Aleksandr Novikov <mem-memov@yandex.ru>
2020-04-20 10:14:41 +0300

    menu

--- showcase/js/modules/panorama.js
+++ showcase/js/modules/panorama.js
@@ -2,6 +2,7 @@ import * as THREE from '/js/threejs/r116/build/three.module.js';
 import {OrbitControls} from '/js/threejs/r116/examples/jsm/controls/OrbitControls.js';
 
 var camera, scene, renderer, controls;
+var backgroundSphereMesh, menuSphereMeshes = [];
 
 export function init(panorama) {
 
@@ -9,22 +10,36 @@ export function init(panorama) {
 
     container = document.getElementById( 'canvas-container' );
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 250, 600 );
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 10, 600 );
     camera.position.set( 0, 0, -50 );
 
 
     scene = new THREE.Scene();
 
-    var geometry = new THREE.SphereBufferGeometry( 500, 30, 30 );
+    var backgroundSphereGeometry = new THREE.SphereBufferGeometry( 500, 30, 30 );
     // invert the geometry on the x-axis so that all of the faces point inward
-    geometry.scale( - 1, 1, 1 );
-
+    backgroundSphereGeometry.scale( - 1, 1, 1 );
     var texture = new THREE.TextureLoader().load( panorama );
     var material = new THREE.MeshBasicMaterial( { map: texture } );
+    backgroundSphereMesh = new THREE.Mesh( backgroundSphereGeometry, material );
+    scene.add( backgroundSphereMesh );
 
-    mesh = new THREE.Mesh( geometry, material );
 
-    scene.add( mesh );
+    var menuSphereGeometry = new THREE.SphereBufferGeometry( 100, 30, 30 );
+    var menuSphereMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, map: texture });
+    var menuSphereMesh = new THREE.Mesh( menuSphereGeometry, menuSphereMaterial );
+    scene.add( menuSphereMesh );
+    menuSphereMesh.position.set(550, 0, 0);
+    menuSphereMesh.visible = false;
+    menuSphereMeshes.push(menuSphereMesh);
+
+
+    var light = new THREE.PointLight( 0xffffff, 2, 1000 );
+    light.position.set( 0, 0, 0 );
+    scene.add( light );
+
+
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -39,6 +54,7 @@ export function init(panorama) {
 
     window.addEventListener( 'resize', onWindowResize, false );
     window.addEventListener( 'wheel', onMouseWheel, false );
+    window.addEventListener( 'click', onMouseClick, false );
 
 }
 
@@ -62,6 +78,14 @@ function onMouseWheel(event) {
     camera.fov += event.deltaY;
     camera.updateProjectionMatrix();
     controls.update();
+}
+
+function onMouseClick() {
+
+    backgroundSphereMesh.visible = !backgroundSphereMesh.visible;
+    menuSphereMeshes.map(function (menuSphereMesh) {
+        menuSphereMesh.visible = !backgroundSphereMesh.visible;
+    });
 }
 
 export function launchAnimation(onAnimate) {
